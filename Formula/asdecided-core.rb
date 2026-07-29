368741dc016847bc99f07cc8904fcc4771d82426
Tom Ballard <tom@armytage.co>
2026-07-29 12:07:55 +0100

    chore(distribution): update asdecided-core to v0.25.1 (#7)

--- Formula/asdecided-core.rb
+++ Formula/asdecided-core.rb
@@ -4,16 +4,16 @@ class AsdecidedCore < Formula
   license "Apache-2.0"
 
   if OS.mac?
-    url "https://github.com/asdecided/core/releases/download/v0.24.1/asdecided-aarch64-apple-darwin.tar.gz"
-    sha256 "e96046c5d39586bf9ed4b7967120abf0ea1cd536651bb6bc3b0c00d01b7f40e6"
+    url "https://github.com/asdecided/core/releases/download/v0.25.1/asdecided-aarch64-apple-darwin.tar.gz"
+    sha256 "07d08e0a1474094be353ad243545a5538456adc553a3fdbc0718f5f045cbff02"
   else
-    url "https://github.com/asdecided/core/releases/download/v0.24.1/asdecided-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c63ed2b44b7ae4db72395e95f30f74eccfc854585a8ccc39b4ed226e4b55cb48"
+    url "https://github.com/asdecided/core/releases/download/v0.25.1/asdecided-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "6f515983159928d4032ff258ff738dd9885a0d87b21f49fa15c651666b647b80"
   end
 
   def install
-    odie "The v0.24.1 macOS release supports Apple Silicon only." if OS.mac? && !Hardware::CPU.arm?
-    odie "The v0.24.1 Linux release supports x86_64 only." if OS.linux? && !Hardware::CPU.intel?
+    odie "The v0.25.1 macOS release supports Apple Silicon only." if OS.mac? && !Hardware::CPU.arm?
+    odie "The v0.25.1 Linux release supports x86_64 only." if OS.linux? && !Hardware::CPU.intel?
 
     bin.install "decided"
     bin.install "decided-mcp"
